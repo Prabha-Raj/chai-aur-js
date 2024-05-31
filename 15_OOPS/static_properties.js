41ce5f717b2045a7cfde2f68979959b36b8f063f
Prabha <prabhacodingworld@gmail.com>
2024-06-01 03:39:09 +0530

    static keyword in js

---
+++ 15_OOPS/static_properties.js
@@ -0,0 +1,27 @@
+// static properties
+
+class User{
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`Username : ${this.username}`)
+    }
+    static createId(){
+        return '234'
+    }
+}
+const hitesh = new User("hitesh")
+hitesh.logMe()
+// console.log(hitesh.createId())
+
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+const iphone = new Teacher("Iphone", "ip@phone.com")
+iphone.logMe()
+// console.log(iphone.createId())
